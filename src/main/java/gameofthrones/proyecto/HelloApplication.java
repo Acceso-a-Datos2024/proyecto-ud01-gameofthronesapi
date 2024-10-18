18b811c68741af95c1c5560659767026042de86a
a23migueldss <miguids00@gmail.com>
2024-10-18 14:11:40 +0200

    docs(add seleccion_documentos.fxml)

--- src/main/java/gameofthrones/proyecto/HelloApplication.java
+++ src/main/java/gameofthrones/proyecto/HelloApplication.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("seleccion_documentos.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 400);
+        stage.setTitle("Elige archivo");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
