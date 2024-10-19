88f8fa12cb2855314ce12db446339b65dfbb879f
mds00 <miguids00@gmail.com>
2024-10-19 20:23:10 +0200

    feat(add methods to switch between scenes)

--- src/main/java/gameofthrones/proyecto/controller/HelloController.java
+++ src/main/java/gameofthrones/proyecto/controller/HelloController.java
@@ -1,5 +1,6 @@
 package gameofthrones.proyecto.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -29,12 +30,26 @@ public class HelloController {
     }
 
     @FXML
-    protected void clickVolver() throws IOException {
+    protected void clickVolver(ActionEvent event) throws IOException {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gameofthrones/proyecto/consultas_tabla.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root);
-            Stage stage = (Stage) (Volver.getScene().getWindow());
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void abrirExportarDocumento(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gameofthrones/proyecto/exportar_documento.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
