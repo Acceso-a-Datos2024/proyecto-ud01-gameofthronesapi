17750d209ac1d3723e7a776fce0f715d712db029
mds00 <miguids00@gmail.com>
2024-10-19 19:16:24 +0200

    docs(add exportar_documento.fxml)

--- src/main/java/gameofthrones/proyecto/HelloApplication.java
+++ src/main/java/gameofthrones/proyecto/HelloApplication.java
@@ -10,15 +10,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("seleccion_documentos.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("exportar_documento.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 400);
-        stage.setTitle("Elige archivo");
+        stage.setTitle("GoT");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
     }
 
     public static void main(String[] args) {
+            //prueba commit con token
         launch();
     }
 }
--- src/main/java/gameofthrones/proyecto/controller/HelloController.java
+++ src/main/java/gameofthrones/proyecto/controller/HelloController.java
@@ -1,14 +1,44 @@
 package gameofthrones.proyecto.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
+
+    @FXML
+    private VBox exportar_documento;
+
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button Volver;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void clickVolver() throws IOException {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) (Volver.getScene().getWindow());
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
