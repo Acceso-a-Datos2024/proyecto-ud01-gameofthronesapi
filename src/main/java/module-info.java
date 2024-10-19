c8f39a6d7e12accbceeaaf949504ac880371cab5
Miguel Da Silva Simón <145230240+mdss00@users.noreply.github.com>
2024-10-19 22:04:49 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module gameofthrones.proyecto {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
     requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
 
     opens gameofthrones.proyecto to javafx.fxml;
     exports gameofthrones.proyecto;
