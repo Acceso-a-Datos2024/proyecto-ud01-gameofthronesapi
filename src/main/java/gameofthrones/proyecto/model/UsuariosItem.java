5ceadc2cade4efd6bd57ed9725cac0eb326f1b62
Dehaz <120736583+Jorgedehaz@users.noreply.github.com>
2024-10-21 09:22:27 +0200

    Add files via upload

---
+++ src/main/java/gameofthrones/proyecto/model/UsuariosItem.java
@@ -0,0 +1,17 @@
+package gameofthrones.proyecto.model;
+
+public class UsuariosItem{
+	private String password;
+	private String usuario;
+
+	public UsuariosItem() {
+	}
+
+	public String getPassword(){
+		return password;
+	}
+
+	public String getUsuario(){
+		return usuario;
+	}
+}
